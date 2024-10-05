f0b362c3bdc0aaf3579428a9d8c165a076205c1e
MdBillG <mdb8854@gmail.com>
2024-10-05 12:28:39 +0530

    working on rreact icons

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,23 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.StethoscopeIcon = exports.HeartIcon = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _client = _interopRequireDefault(require("react-dom/client"));
 require("./index.css");
 var _App = _interopRequireDefault(require("./App"));
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
+var _heart = require("./icons/heart");
+var _stethoscope = require("./icons/stethoscope");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var HeartIcon = exports.HeartIcon = function HeartIcon(props) {
+  return /*#__PURE__*/_react["default"].createElement(_heart.ReactComponent, props);
+};
+var StethoscopeIcon = exports.StethoscopeIcon = function StethoscopeIcon(props) {
+  return /*#__PURE__*/_react["default"].createElement(_stethoscope.ReactComponent, props);
+};
 var root = _client["default"].createRoot(document.getElementById('root'));
 root.render(/*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)));
 
